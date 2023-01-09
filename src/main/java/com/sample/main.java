b5ca47947a1c6c8ceaa193be79e7b46312a5870b
hakim <samouh591@gmail.com>
2023-01-09 08:33:01 +0100

    modi

--- src/main/java/com/sample/main.java
+++ src/main/java/com/sample/main.java
@@ -4,7 +4,26 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String nome="jBPM";		System.out.println(nome);
+		
+		User u=new User("kamilo");
+	   System.out.println(u.getName());
 	}
 
+}
+
+class User{
+	public User(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	String name ;
 }
